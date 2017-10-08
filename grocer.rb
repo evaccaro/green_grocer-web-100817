20e7ff5323b743571405cd9bf57914a726166981
Elisa Vaccaro <erv230@nyu.edu>
2017-10-08 15:12:11 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,30 @@
 def consolidate_cart(cart)
-  # code here
+  new_cart = {}
+  temp = cart.each_with_object(Hash.new(0)) { |item,counts| counts[item] += 1 }
+    temp.each do |item, count|
+      item.each do |name, info|
+        new_cart[name] = info
+        info[:count] = count
+      end
+    end
+    new_cart
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  
+  cart.each do |item, info|
+    if coupons[:item] == item
+      info[:count] = info[:count] - coupons[:num]
+    end
+  end
+  cart["#{coupons[:item]} W/COUPON"] = {:price => coupons[:cost], :clearance => cart[coupons[:item]][:clearance], :count => 1}
+
+  cart.each do |item, info|
+    if info[:count] == 0 
+      cart.delete(item)
+    end
+  end
+    cart 
 end
 
 def apply_clearance(cart)
